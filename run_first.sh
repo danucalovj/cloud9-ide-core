51c71b1673ece4a9c9ac0e639e497cdf1fb0223c
danucalovj <danucalovj@users.noreply.github.com>
2017-10-24 11:24:09 -0700

    Update run_first.sh

--- run_first.sh
+++ run_first.sh
@@ -19,19 +19,5 @@ git clone https://github.com/danucalovj/cloud9-ide-core.git .
 # Install all required NPM modules
 npm install --save
 
-# Setup authentication
-echo "Type a desired username to login to the Cloud9 IDE, followed by [ENTER]:"
-read username
-echo "Type a desired password for your user, followed by [ENTER]:"
-read password
-echo "Enter the IP address you want the IDE web interface to bind to, followed by [ENTER]:"
-read ip_addr
-echo "Enter the port you want the IDE web interface to bind to, followed by [ENTER]:"
-read port_num
-echo "node server.js --listen $ip_addr --port $port_num --auth $username:$password" > start_server.sh
-
-# CHMOD
-chmod +x start_server.sh
-
 # Done
-echo "Done. Run './start_server.sh to start the server. The Cloud9 IDE will run on http://$ip_addr:$port_num, modify the script if you want to change this setting."
+echo "Done. Now run ./configure_cloud9.sh"
